ceca88268468638c5074fe356e21d77a1de4bf52
Ahmadjon2160 <ahmadjonmt2160@gmail.com>
2025-05-09 08:51:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let firstName = prompt("Sizning ismingiz nima?")
+let lastName = prompt("Sizning familiyangiz nima?")
+let ages = +prompt("Necha yoshdasiz?")
+
+
+console.log(firstName );
+console.log(lastName );
+console.log(ages );
+
+
+let district = prompt("qayerda yashaysiz?")
+let email = prompt("Email adresingiz nima?")
+
+console.log(myName + " " + myage + " " + district  + " " + email );
+console.log(`My name is: ${myName} I'm: ${myage} My district: ${district}  My email adress: ${email}` );
+
